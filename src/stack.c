0d21599d4a2072b1c9fe87867e19353f18243ded
MikkelHojbjerg <MikkelHojbjerg1@gmail.com>
2022-10-31 10:32:43 +0100

    trash

--- src/stack.c
+++ src/stack.c
@@ -12,57 +12,20 @@ void initialize(stack* s){
 
 void push(int x, stack* s){
 
-	//Tjekker om stacken er tom, hvis ja blvier der allokeret hukommelse på størrelse af node.
-	if(s->head = NULL){
-		s->head = (malloc(sizeof(node)));
-	}
-
-	//Giver stacken værdien af x
-	s->head->data = x;
 
 }
 
 int pop(stack* s){
 
-	int temp;
 
-	//Går igennem list indtil den sidste
-	while(s->head->next != NULL){
-
-		//Gemmer x's værdi i temp
-		while(s->head->next->next != NULL){
-			temp = s->head->data;
-		}
-
-	//Tildeler y værdien af x
-	s->head->data = temp;
-
-	}
-
-	//Loop til at tømme stack når der skal poppes
-	while(s->head != NULL){
-
-		//Går igennem list indtil den sidste.
-		while(s->head->next != NULL){
-			//Sætter den sidste head i listen til NULL
-			s->head = NULL;
-		}
-	}
-
-
-	return temp;
+	return -1;
 
 }
 
 bool empty(stack* s)
 {
 
-	//Tjekker om stack er tom
-	if(s->head == NULL){
-		return true;
-	}else{
-		return false;
-	}
+
 }
 
 bool full(stack* s) {
